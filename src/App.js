22a56f78028f28a93970d297b0a1f745c16e6025
Apuroop Busetty <apu.busetty@gmail.com>
2020-11-14 04:13:49 +0530

    Added Home Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <div className="overlay">
+    <div className="middle">
+    <h1 className="launch-text">Launch</h1>
+  </div>
+    <div className="row">
+    <div className="column">
+      <img src="/musk1.jpg" style={{width:"100%"}} />
+      <img src="/musk3.jpg" /> 
+      <img src="/musk4.jpg" />
+      <img src="/musk5.jpg" />
+      <img src="/musk8.jpg" />
+      <img src="/musk7.jpg" />
     </div>
+    <div class="column">
+      <img src="/musk3.jpg" />
+      <img src="/musk5.jpg" /> 
+      <img src="/musk8.jpg" />
+      <img src="/musk7.jpg" />
+      <img src="/musk4.jpg" />
+      <img src="/musk10.jpg" />
+      <img src="/musk12.jpg" />
+    </div>
+    <div class="column">
+      <img src="/musk4.jpg" />
+      <img src="/musk8.jpg" /> 
+      <img src="/musk7.jpg" />
+      <img src="/musk12.jpg" />
+      <img src="/musk1.jpg" />
+      <img src="/musk3.jpg" />
+      <img src="/musk12.jpg" />
+      <img src="/musk9.jpg" />
+    
+    </div>
+    <div class="column">
+      <img src="/musk10.jpg" />
+      <img src="/musk11.jpg" /> 
+      <img src="/musk12.jpg" />
+      <img src="/musk13.jpg" />
+      <img src="/musk9.jpg" />
+      <img src="/musk7.jpg" />
+      <img src="/musk12.jpg" />
+      <img src="/musk11.jpg" />
+      <img src="/musk10.jpg" />
+    </div>
+ 
+  </div>
+ 
+  </div>
+  </>
   );
 }
 
